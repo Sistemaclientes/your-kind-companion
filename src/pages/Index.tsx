127982787e2d9a69aa85af7558f817d42fe15243
Sistemaclientes <suprememidias.ok@gmail.com>
2026-03-27 17:38:31 -0300

    Delete src directory

--- src/pages/Index.tsx
+++
@@ -1,9 +0,0 @@
-export default function Index() {
-  return (
-    <div className="min-h-screen bg-surface flex items-center justify-center">
-      <h1 className="text-3xl font-bold text-on-surface">
-        Projeto em branco ✅
-      </h1>
-    </div>
-  );
-}
